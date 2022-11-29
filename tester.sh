466cd120391508542e3cbe3f07c257818f8da562
Kevin <floreskevin51398@gmail.com>
2022-11-28 19:49:32 -0600

    testing script

--- tester.sh
+++ tester.sh
@@ -3,9 +3,16 @@
 # make a list of all files in testpahse folders
 
 PROG_NAME='./mini'
-phase1='./testphase1'
 phase2='./testphase2'
 
+# Set the color variable
+green='\033[0;32m'
+red='\033[0;31m'
+# Clear the color after that
+clear='\033[0m'
+
+rm 'output.txt'
+rm 'pyout.txt'
 
 for file in $phase1 $phase2;
 do
@@ -15,31 +22,22 @@ done > list.txt
 list='list.txt'
 # compare the outputs, if they are the same show color green else show red
 while read line; do
-    echo $line
-    python3 $line
-    ./mini $line
+    name=$(echo $line )
+    actual=$(python3 $line)
+    out=$($PROG_NAME $line)
+    printf "%s\n %s \n\n" "$name" "$actual" >> pyout.txt
+    printf "%s\n %s \n\n" "$name" "$out" >> output.txt
+
+    if [ out = actual ]
+    then
+        echo -e "Test $name ${green}Passed${clear}"
+    else
+        echo -e "Test $name ${red}Failed${clear}"
+    fi
+
 done < $list
 
-# for each file in the list
-while read line
-do
-    # run the program on the file
-    $PROG_NAME $line >> output.txt
-    # get the name of the file
-    name=$(echo $line )
-    # get the expected output
-    python3 $line >> pyout.txt
-    expected=$(cat pyout.txt)
-    # get the actual output
-    actual=$(cat output.txt)
-    # compare the expected and actual outputs
-    if [ "$actual" = "$expected" ]
-    then
-        echo "Test $name passed"
-    else
-        echo "Test $name failed"
-    fi
-done < "$list"
+
 
 # delete the list file
 rm list
@@ -51,3 +49,18 @@ rm list
 exit 0
 
 
+
+
+#     # # get the expected output
+#     # 
+#     # expected=$(echo "pyout.txt")
+#     # # get the actual output
+#     # actual=$(echo "output.txt")
+#     # # compare the expected and actual outputs
+#     # if [ "$actual" = "$expected" ]
+#     # then
+#     #     echo "Test $name passed"
+#     # else
+#     #     echo "Test $name failed"
+#     # fi
+# done < "$list"
